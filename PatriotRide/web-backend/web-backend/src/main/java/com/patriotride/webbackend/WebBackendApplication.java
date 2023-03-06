1ab35e343a90ac0d42377c7a7831ff9c1d68e33b
nicholasterr24 <nicholasterrazas01@gmail.com>
2023-03-05 22:28:13 -0500

    Added Firebase Initializer using Service Account Key (json)

--- PatriotRide/web-backend/web-backend/src/main/java/com/patriotride/webbackend/WebBackendApplication.java
+++ PatriotRide/web-backend/web-backend/src/main/java/com/patriotride/webbackend/WebBackendApplication.java
@@ -1,13 +1,40 @@
 package com.patriotride.webbackend;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@ComponentScan(basePackages = "user")
 @SpringBootApplication
 public class WebBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebBackendApplication.class, args);
+		try {
+			initializeFirebase();
+			System.out.println("Firebase Successfully Initialized");
+		} catch (Exception e) {
+			System.out.println("Firebase Failed to Initialize");
+		}
+	}
+
+	public static void initializeFirebase() throws IOException {
+		Resource resource = new ClassPathResource("serviceAccountKey.json");
+		FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		FirebaseApp.initializeApp(options);
 	}
 
 }
